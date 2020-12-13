2f4c3fbce362cd15f075ea69bd903b243cb0faec
YerkoAndrei-PC <amddgbackup2@gmail.com>
2020-12-13 10:58:54 -0300

    Primer Commit

---
+++ Programa.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Figgle;
+using System.Globalization;
+using System.Timers;
+
+// API BUDA
+//https://api.buda.com/#ticker
+
+public class Programa
+{
+    private string api = "https://www.buda.com/api/v2/markets/btc-clp/ticker";
+    private HttpClient cliente;
+    private HttpResponseMessage respuesta;
+    private string resultado;
+
+    private static Timer timer;
+
+    private static void Main(string[] args)
+    {
+        Console.Title = "                                              --- Cargando ---";
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.SetWindowSize(53, 14);
+        Console.SetBufferSize(53, 14);
+
+        Programa programa = new Programa();
+        programa.ObtenerRespuesta();
+
+        timer = new Timer();
+        timer.Interval = 5000;
+        timer.Elapsed += programa.ObtenerRespuesta;
+        timer.AutoReset = true;
+        timer.Enabled = true;
+        Console.ReadKey();
+    }
+
+    private void ObtenerRespuesta(Object source = null, System.Timers.ElapsedEventArgs e = null)
+    {        
+        cliente = new HttpClient();
+        respuesta = cliente.GetAsync(api).Result;
+        resultado = respuesta.Content.ReadAsStringAsync().Result;
+
+        if (respuesta.IsSuccessStatusCode)
+            Imprimir(JsonConvert.DeserializeObject(resultado));
+        else
+            ImprimirError(JsonConvert.DeserializeObject(resultado));
+    }
+
+    private void Imprimir(dynamic json)
+    {
+        Console.Clear();
+        var textoPrecio = json.ticker.last_price[0].ToString();
+        var valorPrecio = Double.Parse(textoPrecio, CultureInfo.InvariantCulture);
+        var precioalorFormateado = string.Format("{0:C}", valorPrecio);
+
+        Console.Title = "                                              --- Bitcoin ---";
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("*****************************************************");
+
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.Write("                 ");
+        Console.WriteLine("Bitcoin - Peso Chileno");
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("*****************************************************");
+
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine(FiggleFonts.Standard.Render(precioalorFormateado));
+
+        Console.Beep();
+    }
+
+    private void ImprimirError(dynamic json)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Title = "                                              --- Error ---";
+
+        Console.WriteLine("*****************************************************");
+        Console.Write("                    ");
+        Console.WriteLine("Error " + json.code);
+        Console.WriteLine();
+        Console.WriteLine("*****************************************************");
+
+        Console.WriteLine(json.message_code);
+        Console.WriteLine(json.message);
+    }
+}
